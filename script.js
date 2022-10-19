988fa92d796b658dc8f91266494cd331c17cc855
eirol <eiroljohn35@gmail.com>
2022-10-19 18:46:14 +0800

    added main game function, game plays a single round correctly

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 function getComputerChoice() {
     let choiceArray = [
-        "Rock",
-        "Paper",
-        "Scissors"
+        "rock",
+        "paper",
+        "scissors"
     ];
 
     let randomChoice = choiceArray[Math.floor(Math.random() * choiceArray.length)];
-    console.log(randomChoice);
+    console.log("computer: " + randomChoice);
     return randomChoice;
 }
 
 function playerSelection() {
-    let playerChoice = prompt("Choose between rock, paper, or scissors.")
-    console.log(playerChoice);
+    let playerChoice = prompt("Choose between rock, paper, or scissors.");
+    console.log("user: " + playerChoice);
     return playerChoice;
 }
 
 function mainGame(player, computer) {
-    
+    if (player == "rock" && computer == "scissors"){
+        console.log("You win! Rock beats Scissors");
+    } else if (player == "scissors" && computer == "rock"){
+        console.log("You lose! Rock beats Scissors");
+    } else if (player == "paper" && computer == "rock"){
+        console.log("You win! Paper beats Rock")
+    } else if (player == "rock" && computer == "paper"){
+        console.log("You lose! Paper beats Rock")
+    } else if (player == "scissors" && computer == "paper"){
+        console.log("You win! Scissors beats Paper")
+    } else if (player == "paper" && computer == "scissors"){
+        console.log("You lose! Scissors beats Paper")
+    } else if (player == "rock" && computer =="rock") {
+        console.log("You tied!");
+    } else if (player == "scissors" && "scissors") {
+        console.log("You tied!");
+    } else if (player == "paper" && computer == "paper"){
+        console.log("You tied!");
+    }
 }
 
-getComputerChoice();
-playerSelection();
+mainGame(playerSelection(), getComputerChoice());
